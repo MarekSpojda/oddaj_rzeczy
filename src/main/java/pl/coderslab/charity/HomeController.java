389b628ea4c0aee273a15c5ed84ebc16b2a5ae5b
marekspojda <marek.spojda@o2.pl>
2019-08-31 13:00:39 +0200

    Displaying number of bags and institutions based on database added, DonationController added

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,6 +3,7 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import javax.servlet.http.HttpSession;
@@ -11,14 +12,18 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController {
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model, HttpSession session) {
         session.setAttribute("institutions", institutionRepository.findAll());
+        session.setAttribute("allbags", DonationRepository.findAllBags(donationRepository.findAll()));
+        session.setAttribute("allinstitutions", institutionRepository.findAll().size());
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,7 +1,20 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.entity.Donation;
 
+import java.util.List;
+
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+//    @Query("select u from User u where u.id=?1")
+//    int findByIdCustom(Long id);
+
+    static int findAllBags(List<Donation> donations) {
+        int counter = 0;
+        for (Donation donation : donations) {
+            counter += donation.getQuantity();
+        }
+        return counter;
+    }
 }
